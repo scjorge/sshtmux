448edaf8856df5ccd5b8258e785a94d2ddbafc43
Jorge Silva da Costa <jorge.costa@comunix.net.br>
2024-11-01 09:40:11 -0300

    fix import and ruff config

--- sshtmux/sshm/__init__.py
+++ sshtmux/sshm/__init__.py
@@ -1,13 +1,13 @@
-from .ssh_config import SSH_Config # noqa: F401
-from .ssh_graph import generate_graph # noqa: F401
-from .ssh_group import SSH_Group # noqa: F401
+from .ssh_config import SSH_Config  # noqa: F401
+from .ssh_group import SSH_Group  # noqa: F401
 from .ssh_host import SSH_Host  # noqa: F401
 from .ssh_parameters import *  # noqa: F401, F403
-from .sshutils import ( 
-    complete_params, # noqa: F401 
-    complete_ssh_group_names, # noqa: F401
-    complete_ssh_host_names, # noqa: F401
-    complete_styles, # noqa: F401
-    expand_names, # noqa: F401
-    trace_jumphosts, # noqa: F401
+from .ssh_graph import generate_graph  # noqa: F401
+from .sshutils import (
+    complete_params,  # noqa: F401
+    complete_ssh_group_names,  # noqa: F401
+    complete_ssh_host_names,  # noqa: F401
+    complete_styles,  # noqa: F401
+    expand_names,  # noqa: F401
+    trace_jumphosts,  # noqa: F401
 )
